d2822e7d1f3c8ccfd35ed4b231a3f09c1fbdcf51
qiuxan <qiuxan@gmail.com>
2016-07-11 00:04:01 +1000

    link to db and creat a table

--- testing.php
+++ testing.php
@@ -1,6 +1,6 @@
 <?php 
-
-$link = mysqli_connect( 'localhost',  'user_admin',  '11111', 'users'); //connect to the database
+/*
+$link = mysqli_connect( 'localhost',  'user_admin',  '11111', 'user_info'); //connect to the database
 
 if (!$link) {
 	
@@ -9,14 +9,41 @@ if (!$link) {
 	
 }
 
+
+$query="CREATE TABLE `user_info`.`users` ( `number` INT NOT NULL AUTO_INCREMENT , `name` CHAR(11) NOT NULL , `surname` CHAR(11) NOT NULL , `email` CHAR(30) NOT NULL , PRIMARY KEY (`number`), UNIQUE (`email`)) ENGINE = InnoDB;";
+$link->query($query);*/
+
+
 class app{
 	
-	
-	
-    private $filename='', $file;
+	private $filename='', $file;
     private $user_data=array();
     
-
+    public $link;
+	public $query_creat_table="CREATE TABLE `user_info`.`users` ( `number` INT NOT NULL AUTO_INCREMENT , `name` CHAR(11) NOT NULL , `surname` CHAR(11) NOT NULL , `email` CHAR(30) NOT NULL , PRIMARY KEY (`number`), UNIQUE (`email`)) ENGINE = InnoDB;";//set the query as a var for future change if needed
+   
+    
+	public function db_connect_mysqli(){
+		$this->link=mysqli_connect( 'localhost',  'user_admin',  '11111', 'user_info'); //connect to the database
+		if (!$this->link) {
+	
+			printf("Can't connect to MySQL Server. Errorcode: %s ", mysqli_connect_error()); 
+			exit; 
+		}
+		//$this->link->query($this->query_creat_table);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
     public function print_file_name(){
 	    echo 'file name is '.$this->filename;
     }//testing
@@ -59,3 +86,6 @@ $appctrl=new app;//creat an object
 $appctrl->set_file_name($input);
 $appctrl->print_file_name();
 $appctrl->set_file();//save the data of the fiel into an array
+$appctrl->db_connect_mysqli();
+//echo "$appctrl->query_creat_table";
+$appctrl->link->query($appctrl->query_creat_table);
